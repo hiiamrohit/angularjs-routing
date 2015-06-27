676d12b73a354a3c1d0b47c72f6e32f428e7e67d
Rohit Kumar <rohit0kumar@hotmail.com>
2015-06-27 13:40:45 +0530

    v1

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var rohitApp = angular.module('rohitApp',['ngRoute'])
+     .config(function($routeProvider){
+     	$routeProvider
+     	   // routing on home page
+     	   .when('/', {
+     	   	  templateUrl: 'templates/home.html',
+     	   	  controller: 'homeController'
+     	   })
+     	    //routing on about page
+     	  .when('/about', {
+     	   	  templateUrl: 'templates/about.html',
+     	   	  controller: 'aboutController'
+     	   })
+     	   //routing on contact page
+     	   .when('/contactus', {
+     	   	  templateUrl: 'templates/contactus.html',
+     	   	  controller: 'contactusController'
+     	   }) 
+     });
+
+     rohitApp    // where rohitApp is our ng-app module
+     .controller('homeController', function($scope) {
+          $scope.pageTitle = "Home Page";
+          $scope.pageDescription = "I am Home Page."
+     })
+     // calling abou controller
+     .controller('aboutController', function($scope){
+          $scope.pageTitle = "About Page";
+          $scope.pageDescription = "I am About Page."
+     })
+     // calling contact us controller
+      .controller('contactusController', function($scope){
+          $scope.pageTitle = "Contact Us";
+          $scope.pageDescription = "I am Contact Us Page."
+     })
+
+
